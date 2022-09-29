929a468ebf1f465519a1827cf53cc61c06709d29
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-09-29 13:53:55 +0000

    cambio

---
+++ main.py
@@ -0,0 +1,15 @@
+# TODO: define the 'EXPECTED_BAKE_TIME' constant
+
+EXPECTED_BAKE_TIME = 40
+
+# TODO: consider defining the 'PREPARATION_TIME' constant
+
+#       equal to the time it takes to prepare a single layer
+
+PREPARATION_TIME = 2
+
+# TODO: define the 'bake_time_remaining()' function
+
+def bake_time_remaining(elapsed_bake_time):
+
+    """Calculate the bake time remaining.
